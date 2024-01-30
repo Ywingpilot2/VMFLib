cb6b8e4f955c8eaa723e721aff28556c4e87a724
Y wingpilot2 <136618828+Ywingpilot2@users.noreply.github.com>
2024-01-29 20:23:44 -0900

    Update Program.cs

--- VMfTest/Program.cs
+++ VMfTest/Program.cs
@@ -8,7 +8,7 @@ namespace VMfTest
 {
     internal class Program
     {
-        internal List<BaseVClass> VClasses = new List<BaseVClass>();
+        public static List<BaseVClass> VClasses = new List<BaseVClass>();
         public static void Main(string[] args)
         {
             string currentInput = Console.ReadLine();
@@ -31,6 +31,34 @@ namespace VMfTest
                         
                         stopwatch.Stop();
                         Console.WriteLine($"Read {currentInput.Split(' ')[1]} in {stopwatch.ElapsedMilliseconds / 1000.0}s");
+                    } break;
+                    case "readadd":
+                    {
+                        VClassReader classReader = new VClassReader(currentInput.Split(new[] { "\"" }, StringSplitOptions.RemoveEmptyEntries)[1].Trim('"'));
+                        Stopwatch stopwatch = new Stopwatch();
+                        stopwatch.Start();
+                        
+                        BaseVClass currentClass = classReader.ReadClass();
+                        while (currentClass != null)
+                        {
+                            Console.WriteLine($"Successfully read class {currentClass.ClassHeader}!");
+                            VClasses.Add(currentClass);
+                            currentClass = classReader.ReadClass();
+                        }
+                        
+                        stopwatch.Stop();
+                        Console.WriteLine($"Read {currentInput.Split(' ')[1]} in {stopwatch.ElapsedMilliseconds / 1000.0}s");
+                    } break;
+                    case "clear":
+                    {
+                        VClasses.Clear();
+                    } break;
+                    case "printall":
+                    {
+                        foreach (BaseVClass vClass in VClasses)
+                        {
+                            Console.WriteLine(vClass.ToString());
+                        }
                     } break;
                 }
                 currentInput = Console.ReadLine();
